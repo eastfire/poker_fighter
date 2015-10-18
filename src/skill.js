d5a81ec5fbec94424ce17edf1720dd6cd20600e2
eastfire <eastfire_he@hotmail.com>
2015-10-18 15:10:20 +0800

    skills

--- src/skill.js
+++ src/skill.js
@@ -4,6 +4,10 @@ var SkillModel = Backbone.Model.extend({
             name:"no-skill",
             displayName:"无技能",
             charge: 3,
+            maxCharge: 3,
+            durationTime: 10,
+            currentTime: 10,
+            maxCoolDown: 10,
             coolDown: 10,
             description:"牛X的人不需要使用技能"
         }
